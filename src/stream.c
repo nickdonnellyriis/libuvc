d63eee48d2cacbf6256f9cda8832e28944293cb2
Kurt Kiefer <kekiefer@gmail.com>
2016-12-14 19:51:32 -0800

    Fix issues with short data packets

--- src/stream.c
+++ src/stream.c
@@ -595,10 +595,11 @@ void LIBUSB_CALL _uvc_stream_callback(struct libusb_transfer *transfer) {
           continue;
         }
 
-        pktbuf = libusb_get_iso_packet_buffer_simple(transfer, packet_id);
+        if (pkt->actual_length > 0) {
+            pktbuf = libusb_get_iso_packet_buffer_simple(transfer, packet_id);
 
-        _uvc_process_payload(strmh, pktbuf, pkt->actual_length);
-
+            _uvc_process_payload(strmh, pktbuf, pkt->actual_length);
+        }
       }
     }
     break;
@@ -1034,7 +1035,8 @@ void *_uvc_user_caller(void *arg) {
 
     pthread_mutex_unlock(&strmh->cb_mutex);
 
-    strmh->user_cb(&strmh->frame, strmh->user_ptr);
+    if (strmh->hold_bytes == strmh->cur_ctrl.dwMaxVideoFrameSize)
+        strmh->user_cb(&strmh->frame, strmh->user_ptr);
   } while(1);
 
   return NULL; // return value ignored
